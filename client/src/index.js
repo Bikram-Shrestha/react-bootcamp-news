31381aa079c2f44f8eedc25e5827d8d91ba8db84
Bikram Shrestha <bikramshrestha777@gmail.com>
2019-08-30 13:33:17 +1000

    Init commit

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';  
+
+// Render Home.js to the root of the HTML index file.
+ReactDOM.render(<App/>, document.getElementById('root'));
